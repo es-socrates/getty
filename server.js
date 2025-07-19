f5e0952837d8a0fb906f2aee92f196d7a2a296cf
Sócrates <mitnickve@protonmail.com>
2025-07-19 17:55:29 -0400

    New admin design and minor fixes
    - Make logo clickable to homepage in admin.
    - Maintained existing flex layout and styling

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
-  console.log(`🚀Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
 const wss = new WebSocket.Server({ noServer: true });
