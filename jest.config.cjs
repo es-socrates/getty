b9e848daf4b3fc5bff8537ca82f9e4e39a126f1d
Sócrates <mitnickve@protonmail.com>
2025-10-04 16:41:59 -0400

    Minor fixes

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,9 +5,9 @@ module.exports = {
     '^.+\\.vue$': ['@swc/jest', { jsc: { target: 'es2022', parser: { syntax: 'ecmascript', dynamicImport: true } } }]
   },
   moduleFileExtensions: ['js','json','vue'],
+  moduleDirectories: ['node_modules', 'shared-i18n'],
   moduleNameMapper: {
-    '^vue-i18n$': 'vue-i18n/dist/vue-i18n.cjs.js',
-    '^shared-i18n/(.*)$': '<rootDir>/shared-i18n/$1'
+    '^vue-i18n$': 'vue-i18n/dist/vue-i18n.cjs.js'
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(?:\\.pnpm/[^/]+/node_modules/)?(@?vue|vue-i18n|@intlify)/)'
